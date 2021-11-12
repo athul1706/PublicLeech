cd42fd125312f62c86c63ce90d5a413ac09a4eae
Infamous1706 <61957669+athul1706@users.noreply.github.com>
2021-11-12 19:23:40 +0530

    Update custom_thumbnail.py

--- publicleechgroup/plugins/custom_thumbnail.py
+++ publicleechgroup/plugins/custom_thumbnail.py
@@ -16,8 +16,7 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import os
-from PIL import Image
-from publicleechgroup import DOWNLOAD_LOCATION
+from config import DOWNLOAD_LOCATION
 from publicleechgroup.amocmadin import Loilacaztion
 
 
